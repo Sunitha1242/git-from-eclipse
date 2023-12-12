35e6cff0742b5390b327883e90d2844849b7549a
sunitha <sunitharamanim@gmail.com>
2023-12-12 12:30:55 +0530

    ServiceRegistry-Ramya

---
+++ service-registroy/src/test/java/com/ramya/serviceregistroy/ServiceRegistroyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramya.serviceregistroy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistroyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
